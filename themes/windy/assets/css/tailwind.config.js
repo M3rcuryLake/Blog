74e0aaccfc77a6dd3ec15f7c3c53931a5975a64c
M3rcurylake <ankitmukherjee814@gmail.com>
2024-12-07 15:19:59 +0530

    fixes and minimal additions

--- themes/windy/assets/css/tailwind.config.js
+++ themes/windy/assets/css/tailwind.config.js
@@ -3,28 +3,28 @@ module.exports = {
     "./themes/**/layouts/**/*.html",
     "./content/**/layouts/**/*.html",
     "./layouts/**/*.html",
-    "./content/**/*.html"
+    "./content/**/*.html",
   ],
   theme: {
     extend: {
-      fontFamily :{
-        Neucha :['Neucha'],
-        Nunito :['Nunito'],
-        Pacifico :['Pacifico'],
-        PatrickHand :['Patrick Hand'],
-        PermanentMarker :['Permanent Marker'],
-        BebasNeue :['Bebas Neue'],
-        RockSalt : ['Rock Salt'],
-        UbuntuMono : ['Ubuntu Mono'],
-        VT323 : ['VT323'],
-        Fira : ['Fira Mono'],
-      }
+      fontFamily: {
+        Neucha: ["Neucha"],
+        Nunito: ["Nunito"],
+        Pacifico: ["Pacifico"],
+        PatrickHand: ["Patrick Hand"],
+        PermanentMarker: ["Permanent Marker"],
+        BebasNeue: ["Bebas Neue"],
+        RockSalt: ["Rock Salt"],
+        UbuntuMono: ["Ubuntu Mono"],
+        VT323: ["VT323"],
+        Fira: ["Fira Mono"],
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
-  ]
-}
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
