54dd9a6787a31406104f150a2997e9d4c0d9409f
M3rcurylake <ankitmukherjee814@gmail.com>
2024-12-18 01:38:07 +0530

    removed plugins pt.6

--- themes/windy/assets/css/tailwind.config.js
+++ themes/windy/assets/css/tailwind.config.js
@@ -21,9 +21,4 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
 };
